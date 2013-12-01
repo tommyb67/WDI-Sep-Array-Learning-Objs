d58d899e5fd3ab188bda1d219b64533d9983942d
Tom Brennan <tommybrennan@gmail.com.com>
2013-11-30 20:11:31 -0500

    working on uploading the objectives file

--- wdiSepArrayLearningObjs/app/controllers/objectives_controller.rb
+++ wdiSepArrayLearningObjs/app/controllers/objectives_controller.rb
@@ -4,4 +4,8 @@ class ObjectivesController < ApplicationController
     @objectives = Objective.all
   end
 
+  def import
+    Objective.import(params[:file])
+    redirect_to root_url, notice: "Onjectives imported."
+  end
 end
--- wdiSepArrayLearningObjs/config/routes.rb
+++ wdiSepArrayLearningObjs/config/routes.rb
@@ -1,4 +1,6 @@
 WdiSepArrayLearningObjs::Application.routes.draw do
   resources :objectives do
+    collection { post :import }
   end
+  root to: 'objectives#index'
 end
